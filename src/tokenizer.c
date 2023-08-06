0885b3d558e7646510987159e73352f6799a3ef0
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-06 06:45:20 +0200

    Tokenize '..'.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -290,6 +290,12 @@ Token tokenize(Tokenizer* tokenizer) {
         ? token
         : make_error_token(tokenizer, "'@' is only allowed in names of the built-in functionality.");
     }
+    // TODO: Move this condition earlier once TOKEN_DOT is added.
+    case '.': {
+      if (match(tokenizer, '.'))
+        return make_token(tokenizer, TOKEN_DOT_DOT);
+      return make_error_token(tokenizer, "You have included an illegal character: . (This character is only allowed as `..`.)");
+    }
     default:
       if (is_alpha(character))
         return consume_keyword_or_identifier(tokenizer);
