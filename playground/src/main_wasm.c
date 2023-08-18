8e257ad33eaeaeda9952f8c7265f07cfe95cded8
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-18 10:01:17 +0200

    Interpret Thusly code triggered by HTML DOM event.

--- playground/src/main_wasm.c
+++ playground/src/main_wasm.c
@@ -3,25 +3,42 @@
 
 #include <emscripten/emscripten.h>
 
+#include "../../src/exit_code.h"
+#include "../../src/vm.h"
+
 // If this was to be ported to C++, this macro is needed so that it treats it as an external C function.
-#ifdef __cplusplus
-#define EXTERN extern "C"
-#else
-#define EXTERN
-#endif
+// #ifdef __cplusplus
+// #define EXTERN extern "C"
+// #else
+// #define EXTERN
+// #endif
 
 // Using `EMSCRIPTEN_KEEPALIVE` before the function name prevents Emscripten-generated code
 // from eliminating it as dead code. (It always just calls the `main()` function by default.)
-EMSCRIPTEN_KEEPALIVE
+/*EXTERN*/ EMSCRIPTEN_KEEPALIVE
 int run_source(char* code) {
-  // Must always end with newline!
-  printf("'run_source()' called with:\n%s\n", code);
+  VM vm;
+  vm_init(&vm);
+
+  ErrorReport report = interpret(&vm, code);
+
   free(code);
+  vm_free(&vm);
+
+  // Must always end with newline!
+  printf("\n");
+
+  if (report == REPORT_COMPILE_ERROR)
+    return EXIT_CODE_INPUT_DATA_ERROR;
+  if (report == REPORT_RUNTIME_ERROR)
+    return EXIT_CODE_INTERNAL_SOFTWARE_ERROR;
 
   return 0;
 }
 
-int main() {
-  printf("Hey, welcome to the Thusly playground!\n");
-  return 0;
-}
+// Uncomment if not using `--no-entry` on Emscripten compilation and removing
+// `$interpreter_dir/main.c` from the `$files_to_compile`.
+// int main() {
+//   printf("Hey, welcome to the Thusly playground!\n");
+//   return EXIT_SUCCESS;
+// }
