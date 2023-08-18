332cb35eb0ef3a61c98ba669e1b264fa2fd095b7
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-18 10:14:13 +0200

    Move temporary  styles to separate file.

--- build_playground.sh
+++ build_playground.sh
@@ -37,6 +37,7 @@ files_to_compile="$source_dir/main_wasm.c $interpreter_dir/*.c"
 #   (Adding this option until `$interpreter_dir/main.c` can be removed from `$files_to_compile`.)
 emcc $files_to_compile -o $output_dir/index.html -O3 --shell-file $source_dir/playground.html -s NO_EXIT_RUNTIME=1 -s "EXPORTED_RUNTIME_METHODS=['ccall']" --no-entry
 cp $source_dir/*.js $output_dir
+cp $source_dir/*.css $output_dir
 echo "Done!"
 
 echo -e "\nCongratulations! You can start the playground by opening \`$output_dir/index.html\` in the browser via an \`http://\` server (not \`file://\`)."
