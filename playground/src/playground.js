55a84b42079b127de7ff58984027b1fb2b8c7d57
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-19 14:37:47 +0200

    Support choosing a code snippet to start with.

--- playground/src/playground.js
+++ playground/src/playground.js
@@ -1,14 +1,37 @@
 import { editor } from "./editor_setup.js";
+import { snippets } from "./snippets.js";
 
+const runButtonElement = document.getElementById("thusly-run");
 const debugCompilationElement = document.getElementById("debug-compilation");
 const debugExecutionElement = document.getElementById("debug-execution");
+const outputElement = document.getElementById("output");
 
-document.getElementById("run").addEventListener("click", () => {
+const loopsSnippetElement = document.getElementById("loops-snippet");
+const selectionSnippetElement = document.getElementById("selection-snippet");
+const blockSnippetElement = document.getElementById("block-snippet");
+
+runButtonElement.addEventListener("click", () => {
   clearOutput();
   const code = editor.getValue();
   run(code, debugCompilationElement.checked, debugExecutionElement.checked);
 });
 
+loopsSnippetElement.addEventListener("click", () => {
+  showSelectedSnippet(loopsSnippetElement.value);
+});
+
+selectionSnippetElement.addEventListener("click", () => {
+  showSelectedSnippet(selectionSnippetElement.value);
+});
+
+blockSnippetElement.addEventListener("click", () => {
+  showSelectedSnippet(blockSnippetElement.value);
+});
+
+function showSelectedSnippet(name) {
+  editor.setValue(snippets[name]);
+}
+
 function clearOutput() {
-  document.getElementById("output").value = "";
+  outputElement.value = "";
 }
